1a13bdac893a2981badabd9aafa74a8b3c5a668e
Lars Heinitz <lars.heinitz@zem.ch>
2022-02-22 11:11:37 +0100

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {  
+  content: [   
+        "./pages/**/*.{js,ts,jsx,tsx}",   
+        "./components/**/*.{js,ts,jsx,tsx}",  
+         ],
+  theme: {   
+         extend: {
+          transformOrigin: {
+               'camera-on': '38px 202px',
+             }
+         },
+         colors: {
+          secondary: '#B7CAFF',
+        },
+  },  
+  variants: {
+       display: ['children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-not-last', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
+  },
+  plugins: [
+       require('tailwindcss-children'),
+  ],
+}
