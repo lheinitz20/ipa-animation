1af4663a04831e21969ca787bf1732e1ae058e92
Lars Heinitz <lars.heinitz@zem.ch>
2022-03-01 15:24:46 +0100

    first step to responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
          colors: {
           secondary: '#B7CAFF',
         },
+        screens: {
+          'poi': { 'max': '1921px' },
+          // => @media (max-width: 1921px) { ... }
+        },
   },  
   variants: {
        display: ['children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-not-last', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
